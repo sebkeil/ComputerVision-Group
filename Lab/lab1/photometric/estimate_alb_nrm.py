b24f4133345371d51a08577e6b2ee3425d8750ef
sebkeil <62847324+sebkeil@users.noreply.github.com>
2020-09-14 12:37:23 +0200

    Update estimate_alb_nrm.py
    enable color images

--- Lab/lab1/photometric/estimate_alb_nrm.py
+++ Lab/lab1/photometric/estimate_alb_nrm.py
@@ -1,70 +1,85 @@
 
+import numpy as np
+import cv2
+import os
+from utils import *
+from estimate_alb_nrm import estimate_alb_nrm
+from check_integrability import check_integrability
+from construct_surface import construct_surface
+import matplotlib.pyplot as plt
 
-def estimate_alb_nrm( image_stack, scriptV, shadow_trick=True):
-    
-    # COMPUTE_SURFACE_GRADIENT compute the gradient of the surface
-    # INPUT:
-    # image_stack : the images of the desired surface stacked up on the 3rd dimension
-    # scriptV : matrix V (in the algorithm) of source and camera information
-    # shadow_trick: (true/false) whether or not to use shadow trick in solving linear equations
-    # OUTPUT:
-    # albedo : the surface albedo
-    # normal : the surface normal
+print('Part 1: Photometric Stereo\n')
 
-    h, w, _ = image_stack.shape
 
-    # create arrays for 
-    # albedo (1 channel)
-    # normal (3 channels)
-    albedo = np.zeros([h, w])
-    normal = np.zeros([h, w, 3])
-
-    """
-    ================
-    Your code here
-    ================
-    for each point in the image array
-        stack image values into a vector i
-        construct the diagonal matrix scriptI
-        solve scriptI * scriptV * g = scriptI * i to obtain g for this point
-        albedo at this point is |g|
-        normal at this point is g / |g|
-    """
-    np.seterr(divide='ignore', invalid='ignore')
-
-    for x in range(h):
-        for y in range(w):
-            i = image_stack[x][y].T
-            scriptI = np.diag(i)
-
-            # replace NaN values so we do not receive LinAlgError
-            i[np.isnan(i)] = 1
-            scriptI[np.isnan(scriptI)] = 1
-            scriptV[np.isnan(scriptV)] = 1
-
-            A = np.matmul(scriptI, scriptV)  # multiply matrices so we can solve the linear system
-            B = np.matmul(scriptI, i)
-            if shadow_trick:
-                g, residuals, rank, s = np.linalg.lstsq(A, B, rcond=None)  # solve linear algebra system for a constant
-            else:
-                g, residuals, rank, s = np.linalg.lstsq(scriptV,i, rcond=None)
-            g_norm = np.linalg.norm(g)
-            albedo[x][y] = g_norm
-            normal[x,y,:] = np.divide(g,g_norm)
-
-    return albedo, normal
-    
-if __name__ == '__main__':
-    n = 5
-    image_stack = np.zeros([10,10,n])
-    scriptV = np.zeros([n,3])
-    estimate_alb_nrm( image_stack, scriptV, shadow_trick=False)
-
-'''    cur_dir = os.path.dirname(os.path.realpath(__file__))
-    target_dir = '/photometrics_images/SphereGray5'
-    image_dir = cur_dir+target_dir
-    
-
-    [image_stack, scriptV] = load_syn_images(image_dir)
+def photometric_stereo(image_dir='./SphereGray5/'):
+    # obtain many images in a fixed view under different illumination
+    print('Loading images...\n')
+    [image_stack, scriptV] = load_syn_images(image_dir, type='color')
+    # [h, w, n] = image_stack.shape
     [h, w, n] = image_stack.shape
-    [albedo, normals] = estimate_alb_nrm(image_stack, scriptV)'''
+    print('Finish loading %d images.\n' % n)
+
+    # compute the surface gradient from the stack of imgs and light source mat
+    print('Computing surface albedo and normal map...\n')
+    [albedo, normals] = estimate_alb_nrm(image_stack, scriptV, shadow_trick=True)
+
+    # integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
+    print('Integrability checking\n')
+    [p, q, SE] = check_integrability(normals)
+
+    threshold = 0.005;
+    print('Number of outliers: %d\n' % np.sum(SE > threshold))
+    SE[SE <= threshold] = float('nan')  # for good visualization
+
+    # compute the surface height
+    height_map = construct_surface(p, q)
+
+    # show results
+    show_results(albedo, normals, height_map, SE)
+
+
+## Face
+def photometric_stereo_face(image_dir='./yaleB02/'):
+    [image_stack, scriptV] = load_face_images(image_dir)
+    [h, w, n] = image_stack.shape
+    print('Finish loading %d images.\n' % n)
+    print('Computing surface albedo and normal map...\n')
+    albedo, normals = estimate_alb_nrm(image_stack, scriptV)
+
+    # integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
+    print('Integrability checking')
+    p, q, SE = check_integrability(normals)
+
+    threshold = 0.005;
+    print('Number of outliers: %d\n' % np.sum(SE > threshold))
+    SE[SE <= threshold] = float('nan')  # for good visualization
+
+    # compute the surface height
+    height_map = construct_surface(p, q)
+
+    # show results
+    show_results(albedo, normals, height_map, SE)
+
+
+if __name__ == '__main__':
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    target_dir = '/photometrics_images/SphereGray5'
+    image_dir = cur_dir + target_dir
+    #photometric_stereo(image_dir)
+
+    target_dir2 = '/photometrics_images/SphereGray25'
+    image_dir2 = cur_dir + target_dir2
+    #photometric_stereo(image_dir=image_dir2)
+
+    target_dir_mon = '/photometrics_images/MonkeyGray'
+    image_dir_mon = cur_dir + target_dir_mon
+    # photometric_stereo(image_dir=image_dir_mon)
+
+    target_dir_color = '/photometrics_images/SphereColor'
+    image_dir_color = cur_dir + target_dir_color
+    photometric_stereo(image_dir=image_dir_color)
+
+    target_dir3 = '/photometrics_images/yaleB02/'
+    image_dir3 = cur_dir + target_dir3
+    # photometric_stereo_face(image_dir=image_dir3)
+
