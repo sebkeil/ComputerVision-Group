c47e49a9dead09500fdc3c799ef26769ca8fb31c
sebkeil <62847324+sebkeil@users.noreply.github.com>
2020-09-14 12:43:15 +0200

    Update photometric_stereo.py

--- Lab/lab1/photometric/photometric_stereo.py
+++ Lab/lab1/photometric/photometric_stereo.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import cv2
 import os
@@ -5,21 +6,22 @@ from utils import *
 from estimate_alb_nrm import estimate_alb_nrm
 from check_integrability import check_integrability
 from construct_surface import construct_surface
+import matplotlib.pyplot as plt
 
 print('Part 1: Photometric Stereo\n')
 
-def photometric_stereo(image_dir='./SphereGray5/' ):
 
+def photometric_stereo(image_dir='./SphereGray5/'):
     # obtain many images in a fixed view under different illumination
     print('Loading images...\n')
-    [image_stack, scriptV] = load_syn_images(image_dir)
+    [image_stack, scriptV] = load_syn_images(image_dir, type='color')
+    # [h, w, n] = image_stack.shape
     [h, w, n] = image_stack.shape
     print('Finish loading %d images.\n' % n)
 
     # compute the surface gradient from the stack of imgs and light source mat
     print('Computing surface albedo and normal map...\n')
-    [albedo, normals] = estimate_alb_nrm(image_stack, scriptV)
-
+    [albedo, normals] = estimate_alb_nrm(image_stack, scriptV, shadow_trick=True)
 
     # integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
     print('Integrability checking\n')
@@ -27,13 +29,14 @@ def photometric_stereo(image_dir='./SphereGray5/' ):
 
     threshold = 0.005;
     print('Number of outliers: %d\n' % np.sum(SE > threshold))
-    SE[SE <= threshold] = float('nan') # for good visualization
+    SE[SE <= threshold] = float('nan')  # for good visualization
 
     # compute the surface height
-    height_map = construct_surface( p, q )
+    height_map = construct_surface(p, q)
 
     # show results
     show_results(albedo, normals, height_map, SE)
+
 
 ## Face
 def photometric_stereo_face(image_dir='./yaleB02/'):
@@ -49,14 +52,34 @@ def photometric_stereo_face(image_dir='./yaleB02/'):
 
     threshold = 0.005;
     print('Number of outliers: %d\n' % np.sum(SE > threshold))
-    SE[SE <= threshold] = float('nan') # for good visualization
+    SE[SE <= threshold] = float('nan')  # for good visualization
 
     # compute the surface height
-    height_map = construct_surface( p, q )
+    height_map = construct_surface(p, q)
 
     # show results
     show_results(albedo, normals, height_map, SE)
-    
+
+
 if __name__ == '__main__':
-    # photometric_stereo('./SphereGray5/')
-    photometric_stereo_face()
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    target_dir = '/photometrics_images/SphereGray5'
+    image_dir = cur_dir + target_dir
+    #photometric_stereo(image_dir)
+
+    target_dir2 = '/photometrics_images/SphereGray25'
+    image_dir2 = cur_dir + target_dir2
+    #photometric_stereo(image_dir=image_dir2)
+
+    target_dir_mon = '/photometrics_images/MonkeyGray'
+    image_dir_mon = cur_dir + target_dir_mon
+    # photometric_stereo(image_dir=image_dir_mon)
+
+    target_dir_color = '/photometrics_images/SphereColor'
+    image_dir_color = cur_dir + target_dir_color
+    photometric_stereo(image_dir=image_dir_color)
+
+    target_dir3 = '/photometrics_images/yaleB02/'
+    image_dir3 = cur_dir + target_dir3
+    # photometric_stereo_face(image_dir=image_dir3)
+
