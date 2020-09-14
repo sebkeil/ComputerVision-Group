271db384766b36cc1e6a438cb93264f86131d822
sebkeil <62847324+sebkeil@users.noreply.github.com>
2020-09-14 12:35:29 +0200

    Update utils.py
    enable color images

--- Lab/lab1/photometric/utils.py
+++ Lab/lab1/photometric/utils.py
@@ -5,34 +5,47 @@ import cv2
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def load_syn_images(image_dir='./SphereGray5/', channel=0):
+
+def load_syn_images(image_dir='./SphereGray5/', type='greyscale'):      # for color pass 'color'
+
+    channels = [0]
+
+    if type == 'color':
+        channels = [0, 1, 2]
+
     files = os.listdir(image_dir)
-    #files = [os.path.join(image_dir, f) for f in files]
+    # files = [os.path.join(image_dir, f) for f in files]
     nfiles = len(files)
-    
+
     image_stack = None
     V = 0
     Z = 0.5
-    
+
     for i in range(nfiles):
-        # read input image
-        im = cv2.imread(os.path.join(image_dir, files[i]))
-        im = im[:,:,channel]
-        
+
+        # read input image per channel
+        for channel in channels:
+            im = cv2.imread(os.path.join(image_dir, files[i]))
+            im = im[:, :, channel]
+
         # stack at third dimension
         if image_stack is None:
             h, w = im.shape
-            print('Image size (H*W): %d*%d' %(h,w) )
-            image_stack = np.zeros([h, w, nfiles], dtype=int)
-            V = np.zeros([nfiles, 3], dtype=np.float64)
-            
-        image_stack[:,:,i] = im
-        
+            print('Image size (H*W): %d*%d' % (h, w))
+            if len(channels) == 1:                                  # it is a grey image
+                image_stack = np.zeros([h, w, nfiles], dtype=int)
+                V = np.zeros([nfiles, 3], dtype=np.float64)
+            else:                                                   # it is a color image
+                image_stack = np.zeros([h, w, nfiles * 3], dtype=int)
+                V = np.zeros([nfiles * 3, 3], dtype=np.float64)
+
+        image_stack[:, :, i] = im
+
         # read light direction from image name
-        X = np.double(files[i][(files[i].find('_')+1):files[i].rfind('_')])
-        Y = np.double(files[i][files[i].rfind('_')+1:files[i].rfind('.png')])
+        X = np.double(files[i][(files[i].find('_') + 1):files[i].rfind('_')])
+        Y = np.double(files[i][files[i].rfind('_') + 1:files[i].rfind('.png')])
         V[i, :] = [-X, Y, Z]
-        
+
     # normalization
     image_stack = np.double(image_stack)
     min_val = np.min(image_stack)
@@ -40,10 +53,10 @@ def load_syn_images(image_dir='./SphereGray5/', channel=0):
     image_stack = (image_stack - min_val) / (max_val - min_val)
     normV = np.tile(np.sqrt(np.sum(V ** 2, axis=1, keepdims=True)), (1, V.shape[1]))
     scriptV = V / normV
-    
+
     return image_stack, scriptV
-    
-    
+
+
 def load_face_images(image_dir='./yaleB02/'):
     num_images = 64
     filename = os.path.join(image_dir, 'yaleB02_P00_Ambient.pgm')
@@ -51,7 +64,7 @@ def load_face_images(image_dir='./yaleB02/'):
     h, w = ambient_image.shape
 
     # get list of all other image files
-    import glob 
+    import glob
     d = glob.glob(os.path.join(image_dir, 'yaleB02_P00A*.pgm'))
     import random
     d = random.sample(d, num_images)
@@ -61,28 +74,27 @@ def load_face_images(image_dir='./yaleB02/'):
     image_stack = np.zeros([h, w, num_images])
 
     for j in range(num_images):
-        ang[0,j], ang[1,j] = np.double(filenames[j][12:16]), np.double(filenames[j][17:20])
-        image_stack[...,j] = cv2.imread(os.path.join(image_dir, filenames[j]), -1) - ambient_image
+        ang[0, j], ang[1, j] = np.double(filenames[j][12:16]), np.double(filenames[j][17:20])
+        image_stack[..., j] = cv2.imread(os.path.join(image_dir, filenames[j]), -1) - ambient_image
 
-
-    x = np.cos(np.pi*ang[1,:]/180) * np.cos(np.pi*ang[0,:]/180)
-    y = np.cos(np.pi*ang[1,:]/180) * np.sin(np.pi*ang[0,:]/180)
-    z = np.sin(np.pi*ang[1,:]/180)
-    scriptV = np.array([y,z,x]).transpose(1,0)
+    x = np.cos(np.pi * ang[1, :] / 180) * np.cos(np.pi * ang[0, :] / 180)
+    y = np.cos(np.pi * ang[1, :] / 180) * np.sin(np.pi * ang[0, :] / 180)
+    z = np.sin(np.pi * ang[1, :] / 180)
+    scriptV = np.array([y, z, x]).transpose(1, 0)
 
     image_stack = np.double(image_stack)
-    image_stack[image_stack<0] = 0
+    image_stack[image_stack < 0] = 0
     min_val = np.min(image_stack)
     max_val = np.max(image_stack)
     image_stack = (image_stack - min_val) / (max_val - min_val)
-    
+
     return image_stack, scriptV
-    
-    
+
+
 def show_results(albedo, normals, height_map, SE):
     # Stride in the plot, you may want to adjust it to different images
     stride = 1
-    
+
     # showing albedo map
     fig = plt.figure()
     albedo_max = albedo.max()
@@ -91,7 +103,7 @@ def show_results(albedo, normals, height_map, SE):
     print(albedo.shape)
     plt.imshow(albedo, cmap="gray")
     plt.show()
-    
+
     # showing normals as three separate channels
     figure = plt.figure()
     ax1 = figure.add_subplot(131)
@@ -101,31 +113,31 @@ def show_results(albedo, normals, height_map, SE):
     ax3 = figure.add_subplot(133)
     ax3.imshow(normals[..., 2])
     plt.show()
-    
+
     # meshgrid
-    X, Y, _ = np.meshgrid(np.arange(0,np.shape(normals)[0], stride),
-    np.arange(0,np.shape(normals)[1], stride),
-    np.arange(1))
+    X, Y, _ = np.meshgrid(np.arange(0, np.shape(normals)[0], stride),
+                          np.arange(0, np.shape(normals)[1], stride),
+                          np.arange(1))
     X = X[..., 0]
     Y = Y[..., 0]
-    
+
     '''
     =============
     You could further inspect the shape of the objects and normal directions by using plt.quiver() function.  
     =============
     '''
-    
+
     # plotting the SE
-    H = SE[::stride,::stride]
+    H = SE[::stride, ::stride]
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.plot_surface(X,Y, H.T)
+    ax.plot_surface(X, Y, H.T)
     plt.show()
-    
+
     # plotting model geometry
-    H = height_map[::stride,::stride]
+    H = height_map[::stride, ::stride]
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.plot_surface(X,Y, H.T)
+    ax.plot_surface(X, Y, H.T)
     plt.show()
 
